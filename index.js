0c355a8b7fb57c04e79d31e5d3a6822094dfe4ca
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-05-21 18:26:32 +0600

    check and add addition

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ dontenv.config();
 const uri = process.env.MONGODB_URI;
 
 const app = express();
+const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 const JWKS = createRemoteJWKSet(
   new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
@@ -200,7 +211,6 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    
     // await client.close();
   }
 }
